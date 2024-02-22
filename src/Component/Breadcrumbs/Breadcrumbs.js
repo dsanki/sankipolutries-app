57c3843c3a5bc6fb8c48d60ac09899321c652d3e
dsanki <dipaksanki@gmail.com>
2024-02-23 02:20:05 +0400

    Breadcrum module update

---
+++ src/Component/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useLocation,Link } from 'react-router-dom';
+
+
+function Breadcrumbs() {
+
+    const location=useLocation();
+    let currentLink='';
+    const crumbs=location.pathname.split('/')
+    .filter(crumb=>crumb!=='')
+    .map(crumb=>{
+        currentLink=+`/${crumb}`
+        return (
+            <div className='crumb' key={crumb}>
+                <Link to={currentLink}>{crumb}</Link>
+              
+            </div>
+          )
+    })
+  return (
+    <div className="breadcrumbs">
+       {crumbs}
+    </div>
+  )
+}
+
+
+export default Breadcrumbs
