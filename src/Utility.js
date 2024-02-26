5196d6093fa4cd9b3161781d6e749b9535854f23
dsanki <dipaksanki@gmail.com>
2024-02-26 19:10:34 +0400

    Add Utility module

---
+++ src/Utility.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import moment from 'moment';
+
+
+  export const ErrorMessageHandle = (code,showAlert) => {
+    let history = useNavigate();
+    if (code === 401) {
+         history("/login")
+     }
+     else if (code === 404) {
+         showAlert("Data not found!!", "danger")
+     }
+     else {
+         showAlert("Error occurred!!", "danger")
+     }
+ };
+
+ export const CalculateAgeInDays = (date) => {
+    var a = moment(new Date(), 'DD-MM-YYYY');
+    var b = moment(new Date(date), 'DD-MM-YYYY');
+    let days = a.diff(b, 'days');
+    return days;
+  };
+
+  export const CalculateAgeInWeeks = (date) => {
+    var a = moment(new Date(), 'DD-MM-YYYY');
+    var b = moment(new Date(date), 'DD-MM-YYYY');
+    let weeks = a.diff(b, 'week');
+    return weeks;
+  };
+
+
+
+//  export const fetchShedsList = async () => {
+//     fetch(variables.REACT_APP_API + 'ChicksMaster/GetShedList')
+//         .then(response => response.json())
+//         .then(data => {
+//             setShedList(data);
+//         });
+// }
+
+// const fetchShedLotMapList = async () => {
+//     fetch(variables.REACT_APP_API + 'ChicksMaster/GetShedLotMapList')
+//         .then(response => response.json())
+//         .then(data => {
+//             setShedLotMapList(data);
+//         });
+// }
+
+ 
